9684a9ec003413807ff5adb277bee153441d5f6d
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2022-09-02 16:50:57 +0200

    Use types from stdint.h instead of custom ones.

--- src/fat/common.h
+++ src/fat/common.h
@@ -36,14 +36,14 @@
 #include "compiler.h"
 
 
-  typedef unsigned long u32;
-  typedef long s32;
+typedef uint32_t u32;
+typedef int32_t s32;
 
-  typedef unsigned short u16;
-  typedef short s16;
+typedef uint16_t u16;
+typedef int16_t s16;
 
-  typedef unsigned char u8;
-  typedef char s8;
+typedef uint8_t u8;
+typedef int8_t s8;
 
 #if !defined(__cplusplus)
   typedef int bool;
